ce61c6b53f0f8f59511d56d9f7e593ad9f14c5fb
greenemax <mjgreene27@gmail.com>
2020-09-08 19:48:02 -0400

    cleaned up js click functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,6 +11,10 @@ $(() => {
     $('#button_odd').click(function () {
       $('.even').hide()
       $('.odd').show()
+      $('#button_odd').css('background', '#fdecca')
+      $('#button_odd').css('color', '#696579')
+      $('#button_even').css('background', '#696579')
+      $('#button_even').css('color', '#fdecca')
     })
   })
 
@@ -18,6 +22,23 @@ $(() => {
     $('#button_even').click(function () {
       $('.odd').hide()
       $('.even').show()
+      $('#button_even').css('background', '#fdecca')
+      $('#button_even').css('color', '#696579')
+      $('#button_odd').css('color', '#fdecca')
+      $('#button_odd').css('background', '#696579')
+      $('#button_all').css('color', '#fdecca')
+      $('#button_all').css('background', '#696579')
+    })
+  })
+
+  $(document).ready(function () {
+    $('#button_all').click(function () {
+      $('#button_all').css('background', '#fdecca')
+      $('#button_all').css('color', '#696579')
+      $('#button_odd').css('color', '#fdecca')
+      $('#button_odd').css('background', '#696579')
+      $('#button_even').css('color', '#fdecca')
+      $('#button_even').css('background', '#696579')
     })
   })
 
@@ -26,6 +47,4 @@ $(() => {
       $('.flex-item').show()
     })
   })
-
-
 })
